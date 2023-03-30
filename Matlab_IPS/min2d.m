e70eb78115a374a79033b2a0e8ac3b5076685b55
keigo728 <127797346+keigo728@users.noreply.github.com>
2023-03-30 16:51:18 +0900

    Delete Matlab_IPS directory
    delete

--- Matlab_IPS/min2d.m
+++
@@ -1,6 +0,0 @@
-function [mi,px,py] = min2d(data)
-
-[ma,px,py] = max2d(data);
-[tmp,px,py] = max2d(ma-data);
-
-mi = -tmp + ma;
--- Matlab_IPS/principalPhase.m
+++
@@ -1,4 +0,0 @@
-function phs = principalPhase( data )
-% ーπ≦θ≦＋πの範囲の位相に直す（主値にする）
-
-phs = angle( exp(i*data) );
